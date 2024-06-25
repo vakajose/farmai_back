a8a55b65e49b673790cff36e0f51f4eba31aac9e
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-06-25 14:49:51 -0400

    servicio auxlixiar de prueba

--- routers/analisis.py
+++ routers/analisis.py
@@ -1,6 +1,8 @@
+import os
+
 from fastapi import APIRouter, HTTPException, status
 from models import Analisis, Parcela
-from schemas import AnalisisCreate, AnalisisResponse,ImagenSatelital
+from schemas import AnalisisCreate, AnalisisResponse, ImagenSatelital
 from typing import List
 from services import sentinelhub, openai
 
@@ -44,18 +46,20 @@ def read_all_analisis(usuario_id: str, parcela_id: str):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-@router.put("/{usuario_id}/{parcela_id}/{analisis_id}", response_model=AnalisisResponse, summary="Actualizar un analisis por ID",
+@router.put("/{usuario_id}/{parcela_id}/{analisis_id}", response_model=AnalisisResponse,
+            summary="Actualizar un analisis por ID",
             description="Actualizar los detalles de un analisis especifico por su ID")
 def update_analisis(usuario_id: str, parcela_id: str, analisis_id: str, analisis: AnalisisCreate):
     try:
         existing_analisis = Analisis.get_by_id(usuario_id, parcela_id, analisis_id)
         if existing_analisis:
-            update_analisis = Analisis(**analisis.dict(),id=analisis_id)
+            update_analisis = Analisis(**analisis.dict(), id=analisis_id)
             update_analisis.save(usuario_id, parcela_id)
             return update_analisis
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Analisis no encontrado")
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
 
 @router.delete("/{usuario_id}/{parcela_id}/{analisis_id}", summary="Eliminar un analisis por ID",
                description="Eliminar un analisis especifico por su ID")
@@ -77,10 +81,10 @@ def get_last_analisis_by_tipo(usuario_id: str, parcela_id: str, tipo: str):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Analisis no encontrado")
 
 
-@router.get("/ejecutar/{usuario_id}/{parcela_id}/{tipo}", response_model=List[ImagenSatelital],
+@router.get("/ejecutar/{usuario_id}/{parcela_id}/{tipo}", response_model=AnalisisResponse,
             summary="Ejecutar un analisis",
             description="Ejecuta el analisis para una parcela especifica y devuelve el resultado del diagnostico")
-def ejecutar_analisis(usuario_id: str, parcela_id: str, tipo: str):
+def ejecutar_analisis(usuario_id: str, parcela_id: str, tipo: str = 'identificacion_plagas'):
     #Traer las imagenes en funcion del tipo de analisis desde sentinelHub
     try:
         sentinel_hub_service = sentinelhub.SentinelHubService()
@@ -93,11 +97,19 @@ def ejecutar_analisis(usuario_id: str, parcela_id: str, tipo: str):
     if imagenes:
         nuevo_analisis = Analisis(tipo=tipo, imagenes=imagenes)
         nuevo_analisis.save(usuario_id, parcela_id)
-        return imagenes
-    #     respuesta = openai.analyze_images(tipo, imagenes)
-    #     #si existe respuesta, guardar el analisis
-    #     if respuesta:
-    #         analisis = Analisis(tipo=tipo, imagenes=imagenes, evaluacion=respuesta)
-    #         analisis.save(usuario_id, parcela_id)
-    #         return respuesta
 
+        respuesta = openai.analyze_images(tipo, imagenes)
+
+        #si existe respuesta, actualizar el analisis
+        if respuesta:
+            nuevo_analisis.evaluacion = respuesta
+            nuevo_analisis.save(usuario_id, parcela_id)
+            return nuevo_analisis
+
+    @router.get("/ejecutarplaga/{usuario_id}/{parcela_id}/", response_model=str,
+                summary="Ejecutar un analisis",
+                description="Ejecuta el analisis para una parcela especifica y devuelve el resultado del diagnostico")
+    def ejecutar_analisis_plaga(usuario_id: str, parcela_id: str):
+        url = os.environ.get('CDN_URL', 'http://cdn.vakajose.live')
+        respuesta = openai.analyze_images_b([f'{url}/nir', f'{url}/red', f'{url}/false_color'])
+        return respuesta
--- services/sentinelhub.py
+++ services/sentinelhub.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import requests
-from models import Parcela, Analisis, ImagenSatelital
+from models import Parcela, Analisis, ImagenSatelital, Punto
 from services.storage import StorageService
 from dotenv import load_dotenv
 import os
@@ -31,12 +31,21 @@ class SentinelHubService:
         response.raise_for_status()
         return response.json()['access_token']
 
+
+    def convert_points_to_coordinates(self,points: List[Punto]):
+        coordinates = []
+        for point in points:
+            coordinates.append([point.longitude, point.latitude])
+        # Append the first point at the end to close the polygon
+        coordinates.append(coordinates[0])
+        return [coordinates]
+
     def fetch_images(self, parcela: Parcela, tipo_analisis: str):
         # Mapear tipo_analisis a bandas específicas
         bandas_map = {
             'estado_vegetacion': ['B04', 'B08'],  # NDVI
             'stress_hidrico': ['B11', 'B12'],  # SWIR bands
-            'identificacion_plagas': ['B02', 'B03', 'B04', 'B08'],  # RGB + NIR
+            'plagas': ['B02', 'B03', 'B04', 'B08'],  # RGB + NIR
             'deteccion_enfermedades': ['B02', 'B03', 'B04', 'B08'],  # RGB + NIR
             'analisis_suelo': ['B02', 'B03', 'B04', 'B08', 'VV', 'VH'],  # RGB + NIR + Radar
             'monitoreo_crecimiento': ['B02', 'B03', 'B04', 'B08'],  # RGB + NIR
@@ -50,10 +59,12 @@ class SentinelHubService:
         if not bandas:
             raise ValueError(f"Tipo de análisis no soportado: {tipo_analisis}")
 
-        polygon_coords = [(punto.longitude, punto.latitude) for punto in parcela.ubicacion]
-        polygon_coords.append(polygon_coords[0])  # Cerrar el polígono
+        # polygon_coords = [(punto.longitude, punto.latitude) for punto in parcela.ubicacion]
+        # polygon_coords.append(polygon_coords[0])  # Cerrar el polígono
 
-        img_prefix = f'{datetime.now().strftime('%Y%m%d')}_{tipo_analisis}-{parcela.id}-{parcela.usuario_id}'
+        polygon_coords = self.convert_points_to_coordinates(parcela.ubicacion)
+
+        img_prefix = f'{datetime.now().strftime('%Y%m%d')}_{tipo_analisis}_{parcela.id}_{parcela.usuario_id}'
 
         images = self._fetch_images_from_sentinel(polygon_coords, bandas,img_prefix,tipo_analisis)
         return images
@@ -65,45 +76,7 @@ class SentinelHubService:
             'Authorization': f'Bearer {self.access_token}',
         }
 
-        # payload = {
-        #     "input": {
-        #         "bounds": {
-        #             "geometry": {
-        #                 "type": "Polygon",
-        #                 "coordinates": [polygon_coords]
-        #             }
-        #         },
-        #         "data": [
-        #             {
-        #                 "type": "sentinel-2-l2a",
-        #                 "dataFilter": {
-        #                     "timeRange": {
-        #                         "from": "2024-05-23T00:00:00Z",
-        #                         "to": "2024-06-24T23:59:59Z"
-        #                     }
-        #                 }
-        #             }
-        #         ]
-        #     },
-        #     "output": {
-        #         "width": 512,
-        #         "height": 512,
-        #         "responses": [
-        #             {
-        #                 "identifier": f"band_{band}",
-        #                 "format": {
-        #                     "type": "image/png"
-        #                 },
-        #                 "evalscript": f"return [{band}];"
-        #             } for band in bandas
-        #         ]
-        #     }
-        # }
-
-
-        payload  = _get_data_by_tipo(tipo_analisis, polygon_coords,img_prefix)
-
-
+        payload = _get_data_by_tipo(tipo_analisis, polygon_coords,img_prefix)
         response = requests.post(url, headers=headers, json=payload)
         if response.status_code == 401:
             # Token expirado, obtener uno nuevo y reintentar
@@ -117,12 +90,13 @@ class SentinelHubService:
 
         if response.status_code == 200:
             try:
-                saved_images = List[ImagenSatelital]
+                saved_images = []
                 for idx, band in enumerate(bandas):
                     filename = f'band_{band}-{img_prefix}.png'
                     storage_service = StorageService()
                     filepath = storage_service.save_image(response.content, filename)
-                    saved_images.append(ImagenSatelital(ruta=filepath, tipo=band))
+                    imagen = ImagenSatelital(ruta=filepath, tipo=band)
+                    saved_images.append(imagen)
                 return saved_images
             except ValueError as e:
                 raise Exception(f"Error parsing JSON response: {e}")
@@ -183,7 +157,7 @@ def _get_data_by_tipo(tipo: str, polygon_coords,img_prefix):
               };
             }
             """
-    elif tipo == 'identificacion_plagas':
+    elif tipo == 'plagas':
         return {
           "input": {
             "bounds": {
@@ -209,36 +183,36 @@ def _get_data_by_tipo(tipo: str, polygon_coords,img_prefix):
             "height": 645.157,
             "responses": [
               {
-                "identifier": f"blue-{img_prefix}",
+                "identifier": f"blue_{img_prefix}",
                 "format": {
                   "type": "image/png"
                 }
               },
               {
-                "identifier": f"green-{img_prefix}",
+                "identifier": f"green_{img_prefix}",
                 "format": {
                   "type": "image/png"
                 }
               },
               {
-                "identifier": f"red-{img_prefix}",
+                "identifier": f"red_{img_prefix}",
                 "format": {
                   "type": "image/png"
                 }
               },
               {
-                "identifier": f"nir-{img_prefix}",
+                "identifier": f"nir_{img_prefix}",
                 "format": {
                   "type": "image/png"
                 }
               },
               {
-                "identifier": f"combined-{img_prefix}",
+                "identifier": f"combined_{img_prefix}",
                 "format": {
                   "type": "image/png"
                 }
               }
             ]
           },
-          "evalscript": "//VERSION=3\n\nfunction setup() {\n  return {\n    input: [\"B02\", \"B03\", \"B04\", \"B08\"],\n    output: [\n      { id: \"blue\", bands: 1 },\n      { id: \"green\", bands: 1 },\n      { id: \"red\", bands: 1 },\n      { id: \"nir\", bands: 1 },\n      { id: \"combined\", bands: 3 }\n    ]\n  };\n}\n\nfunction evaluatePixel(sample) {\n  let blue = sample.B02;\n  let green = sample.B03;\n  let red = sample.B04;\n  let nir = sample.B08;\n\n  return {\n    blue: [blue],\n    green: [green],\n    red: [red],\n    nir: [nir],\n    combined: [nir, red, green]\n  };\n}\n"
+          "evalscript": "//VERSION=3\n\nfunction setup() {\n  return {\n    input: [\"B02\", \"B03\", \"B04\", \"B08\"],\n    output: [\n      { id: \""+f"blue_{img_prefix}"+"\", bands: 1 },\n      { id: \""+f"green_{img_prefix}"+"\", bands: 1 },\n      { id: \""+f"red_{img_prefix}"+"\", bands: 1 },\n      { id: \""+f"nir_{img_prefix}"+"\", bands: 1 },\n      { id: \""+f"combined_{img_prefix}"+"\", bands: 3 }\n    ]\n "+" };\n}\n\nfunction evaluatePixel(sample) {\n  let blue = sample.B02;\n  let green = sample.B03;\n  let red = sample.B04;\n  let nir = sample.B08;\n\n  return {\n    "+f"blue_{img_prefix}: [blue],\n    green_{img_prefix}: [green],\n    red_{img_prefix}: [red],\n    nir_{img_prefix}: [nir],\n    combined_{img_prefix}"+": [nir, red, green]\n  };\n}\n"
         }
